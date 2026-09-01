80391cbd3993ae059caa9f0c99711f25c28d98a9
Adrian <aguirreadriann@gmail.com>
2026-09-01 15:24:49 -0300

    Practica 2 Ej 1 completado

--- Practica 2/Ejercicio 1/script.js
+++ Practica 2/Ejercicio 1/script.js
@@ -5,18 +5,25 @@ formulario.addEventListener("submit", function(event) {
     // evita que la pagina se recargue automaticamente al enviar
     event.preventDefault();
 
-    // obtiene el valor ingresado en el campo "nombre"
+    // obtener el valor ingresado en el campo
     const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const edad = document.getElementById("edad").value;
 
-    // realiza la peticion POST al endpoint simulado
+    //ALmacenar los datos en un obj. de js
+    const usuario = {
+        nombre: nombre,
+        email: email,
+        edad: parseInt(edad) // parsea el texto a numero
+    };
+
+    // realiza la peticion POST enviando el objeto completo
     fetch("/api/usuarios", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({
-            nombre: nombre
-        })
+        body: JSON.stringify(usuario) // enviamos el obj. "usuario" completo como json
     })
     .then(response => response.json())
     .then(data => {
